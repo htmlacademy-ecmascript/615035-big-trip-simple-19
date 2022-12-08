86f931d0a230a3e39c9846bbf782d171205da62a
DimaObora <obora.dima@yandex.ru>
2022-12-08 20:15:18 +0300

    finished presenter

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
 import FilterView from './view/filter-view.js';
 import SortView from './view/sort-view.js';
-import AddNewPointView from './view/add-new-point-view.js';
-import EditePointView from './view/edit-point-view.js';
-import PointView from './view/point-view.js';
 import {render} from './render.js';
-
+import BoardPresenter from './presenter/board-presenter.js';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteBodyElement = document.querySelector('.trip-events');
-
+const boardPresenter = new BoardPresenter({boardContainer: siteBodyElement});
 
 render(new FilterView(), siteFilterElement);
 render(new SortView(), siteBodyElement);
-render(new AddNewPointView(), siteBodyElement);
-render(new EditePointView(), siteBodyElement);
-render(new PointView(), siteBodyElement);
 
-
+boardPresenter.init();
